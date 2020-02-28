0d931ac02252e76e7158178481430a11fef04875
Jeff Maughan <geophray@gmail.com>
2020-02-28 15:24:58 -0700

    Saving my work to be able to work on this remotely.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // Check for type of obj
+  var objType = typeof obj;
+  // Create variable to hold stringified obj
+  var stringified = "";
+  // If obj is a string
+  if (objType === "string") {
+    // Wrap it in quotes and return it
+    stringified += '"' + obj + '"';
+  } else if (objType === "number" || !obj || obj === true) {
+    // If obj is a number, falsey, or true add it to stringified
+    stringified += obj;
+  } else if (objType === "object") {
+    // If obj is of type object
+    // Check if it is an array
+    if (obj instanceof Array) {
+      // Create substring for stringifying the elements of the array
+      var substring = "[";
+      for (let i = 0; i < obj.length; i++) {
+        // Add each element to the string
+        substring += stringifyJSON(obj[i]);
+      }
+      substring += "]";
+      stringified += substring;
+    } else if (obj instanceof Object) {
+      var substring = "[";
+      for (prop in obj) {
+        let property = stringifyJSON(prop);
+        let value = stringifyJSON(obj[prop]);
+        substring += property + ": " + value + ";"
+      }
+      substring += "]";
+    }
+  }
+  return stringified;
 };
