094dd792bc9c9650c6f8b1b8864ba96f611a01fb
Jeff Maughan <geophray@gmail.com>
2020-03-01 00:06:23 -0700

    Good start to getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,26 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // Create array for elements with class
+  var elementsWithClassName = [];
+
+  var checkBranch = function(node) {
+    var classList = node.classList;
+    for( let i = 0; i < classList.length; i++ ) {
+      if( classList[i] === className ) {
+        elementsWithClassName.push(node);
+      }
+    }
+    if( node.hasChildNodes() ) {
+      var children = node.childNodes;
+      for ( let j = 0; j < children.length; j++) {
+        checkBranch(children[j]);
+      }
+    }
+  }
+
+  var body = document.body;
+  checkBranch(body);
+
+  return elementsWithClassName;
 };
