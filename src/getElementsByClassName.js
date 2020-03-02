14ee32512611e52f0eaa16064addbbcabfa01cca
Jeff Maughan <geophray@gmail.com>
2020-03-02 09:51:59 -0700

    Complete getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,28 +4,38 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // Create array for elements with class
   var elementsWithClassName = [];
-
+  //Define function for checking class list on current element and child nodes
   var checkBranch = function(node) {
+    // Load classList of current element
     var classList = node.classList;
-    for( let i = 0; i < classList.length; i++ ) {
-      if( classList[i] === className ) {
-        elementsWithClassName.push(node);
+    // If classList exists
+    if( classList ) {
+      // Iterate over classes in classList
+      for( let i = 0; i < classList.length; i++ ) {
+        // If current class === className
+        if( classList[i] === className ) {
+          // Add it to elementsWithClassName
+          elementsWithClassName.push(node);
+        }
       }
-    }
-    if( node.hasChildNodes() ) {
-      var children = node.childNodes;
-      for ( let j = 0; j < children.length; j++) {
-        checkBranch(children[j]);
+      // If element has children
+      if( node.hasChildNodes() ) {
+        // Load children 
+        var children = node.childNodes;
+        // Iterate over children
+        for ( let j = 0; j < children.length; j++) {
+          // Check each of their classes recursively
+          checkBranch(children[j]);
+        }
       }
     }
   }
-
+  // Invoke the checkBranch function on the body element
   var body = document.body;
   checkBranch(body);
-
+  // Return elementsWithClassName
   return elementsWithClassName;
 };
